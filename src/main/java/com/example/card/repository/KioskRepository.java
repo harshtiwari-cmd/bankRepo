776cfa2b0bf8e04e315bc3690d97267549dc3a9a
Abcom <tanveer.rayeen@omnicoredigital.com>
2025-09-14 20:13:37 +0530

    added exception on branchId if exits

--- src/main/java/com/example/card/repository/KioskRepository.java
+++ src/main/java/com/example/card/repository/KioskRepository.java
@@ -4,7 +4,11 @@ import com.example.card.constrants.model.Kiosk;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface KioskRepository extends JpaRepository<Kiosk, Long> {
+
+    Optional<Kiosk> findByKioskId(String kioskId);
 }
 
--- src/main/java/com/example/card/services/impl/KioskServiceImpl.java
+++ src/main/java/com/example/card/services/impl/KioskServiceImpl.java
@@ -4,11 +4,14 @@ import com.example.card.constrants.mapper.KioskMapper;
 import com.example.card.constrants.model.Kiosk;
 import com.example.card.dto.KioskRequestDTO;
 import com.example.card.dto.KioskResponseDTO;
+import com.example.card.exceptions.KioskAlreadyExistsException;
 import com.example.card.repository.KioskRepository;
 import com.example.card.services.KioskService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class KioskServiceImpl implements KioskService {
@@ -25,6 +28,12 @@ public class KioskServiceImpl implements KioskService {
 
     @Override
     public KioskResponseDTO createKiosk(KioskRequestDTO kioskDto) {
+
+        Optional<Kiosk> kioskId = kioskRepository.findByKioskId(kioskDto.getKioskId());
+
+        if (kioskId.isPresent()) {
+            throw new KioskAlreadyExistsException("Kiosk with ID '" + kioskDto.getKioskId() + "' already exists.");
+        }
 
         Kiosk kiosk = kioskMapper.toEntity(kioskDto);
         if (kiosk.getHolidayCalendar() != null) {
