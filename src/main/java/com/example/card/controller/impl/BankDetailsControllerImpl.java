546154643a2f0287faba52b314c6d2c5f68f954f
omnicore <ashish.parida@omnicoredigital.com>
2025-09-15 18:23:07 +0530

    Controller interface deleted

--- src/main/java/com/example/card/controller/impl/BankDetailsControllerImpl.java
+++ src/main/java/com/example/card/controller/impl/BankDetailsControllerImpl.java
@@ -1,11 +1,9 @@
 package com.example.card.controller.impl;
 
-import com.example.card.controller.BankDetailsController;
 import com.example.card.dto.BankDetailsDto;
 import com.example.card.entity.BankDetailsEntity;
 import com.example.card.exceptions.ResourceNotFoundException;
 import com.example.card.services.BankDetailsService;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/bank-details/api")
 @RestController
-public class BankDetailsControllerImpl implements BankDetailsController {
+public class BankDetailsControllerImpl  {
 
     private final BankDetailsService bankDetailsService;
 
@@ -24,7 +22,6 @@ public class BankDetailsControllerImpl implements BankDetailsController {
     }
 
     @PostMapping
-    @Override
     public ResponseEntity<BankDetailsEntity> saveBankDetails(@RequestBody BankDetailsDto dto) throws ResourceNotFoundException {
         return new ResponseEntity<>(bankDetailsService.createBankDetails(dto), HttpStatus.CREATED);
     }
