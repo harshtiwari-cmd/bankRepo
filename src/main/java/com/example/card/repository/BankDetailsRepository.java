51846f0e1643e5ebb1b032b224a40b13ba9f1dea
omnicore <ashish.parida@omnicoredigital.com>
2025-09-15 14:03:43 +0530

    Implemented entity and repo

---
+++ src/main/java/com/example/card/repository/BankDetailsRepository.java
@@ -0,0 +1,8 @@
+package com.example.card.repository;
+
+import com.example.card.entity.BankDetailsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankDetailsRepository extends JpaRepository<BankDetailsEntity,Long> {
+
+}
